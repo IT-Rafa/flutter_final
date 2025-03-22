cbe38f40004f5004d76360013f59e1b4616ed6bb
Rafael Martínez Costas <it-rafamartinez@outlook.es>
2025-03-22 19:26:45 +0100

    translate done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_final/pages/login_all.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+Future<void> main() async {
+  var delegate = await LocalizationDelegate.create(
+    fallbackLocale: 'en',
+    supportedLocales: ['en', 'es', 'fa'],
+  );
   runApp(
     /// [MyApp] is wrapped in a [ProviderScope].
     /// This widget is where the state of most of our providers will be stored.
     /// This replaces `MultiProvider` if you've used `provider` before.
-    const ProviderScope(child: MainApp()),
+    ProviderScope(child: LocalizedApp(delegate, MainApp())),
   );
 }
 
@@ -16,6 +22,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginAll());
+    var localizationDelegate = LocalizedApp.of(context).delegate;
+
+    return LocalizationProvider(
+      state: LocalizationProvider.of(context).state,
+      child: MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          localizationDelegate,
+        ],
+        supportedLocales: localizationDelegate.supportedLocales,
+        locale: localizationDelegate.currentLocale,
+
+        onGenerateTitle: (context) => translate('app_bar.title'),
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: LoginAll(),
+      ),
+    );
   }
 }
--- lib/pages/login_all.dart
+++ lib/pages/login_all.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 
 class LoginAll extends HookConsumerWidget {
   const LoginAll({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(body: Center(child: Text('Hello World!')));
+    return Scaffold(
+      appBar: AppBar(title: Text(translate('app_bar.title'))),
+      body: Center(child: Text(translate('app_bar.title'))),
+    );
   }
 }
