5ad0c1e3db74ad1415152ec9e2ee96cc57c049ee
Rafael Martínez Costas <it-rafamartinez@outlook.es>
2025-04-03 22:09:12 +0200

    added splash

--- lib/core/routes/go_router_provider.dart
+++ lib/core/routes/go_router_provider.dart
@@ -1,15 +1,21 @@
 import 'package:flutter_final/features/auth/presentation/ui/pages/login_all_screen/login_all_screen.dart';
+import 'package:flutter_final/features/auth/presentation/ui/pages/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
-    initialLocation: '/loginAllScreen',
+    initialLocation: '/splash',
     debugLogDiagnostics: true,
     routes: [
       GoRoute(
+        name: "splash",
+        path: '/splash',
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
         name: "login",
-        path: '/loginAllScreen',
+        path: '/login',
         builder: (context, state) => const LoginAllScreen(),
       ),
     ],
--- lib/features/auth/presentation/ui/pages/splash_screen.dart
+++ lib/features/auth/presentation/ui/pages/splash_screen.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
+class SplashScreen extends ConsumerStatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Simulate a delay for the splash screen
+    Future.delayed(const Duration(seconds: 3), () {
+      // Check if the widget is still mounted before navigating
+      if (mounted) {
+        // Navigate to the login screen after the delay
+        context.go('/login');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(child: Text('Splash Screen')),
+    );
+  }
+}
